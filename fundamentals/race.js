b3143aff2c87cfebe8e52bf95d06b0af55f76cde
Hung Nguyen <nvhung2711@gmail.com>
2025-06-26 15:06:15 +0930

    initial

---
+++ fundamentals/race.js
@@ -0,0 +1,2 @@
+setTimeout(() => console.log('🐇 finishes'), 100);
+console.log('🐢 finishes');
---
+++ package-example/request.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+
+axios.get('https://google.com')
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((err) => console.log(err))
