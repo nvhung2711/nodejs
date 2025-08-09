0e005dd6ed65354074e4f39ab9207f519c15b36c
Hung Nguyen <nvhung2711@gmail.com>
2025-08-10 08:09:43 +0930

    fix the indentation and format making the code easier to read

--- fundamentals/events.js
+++ fundamentals/events.js
@@ -4,14 +4,14 @@ const celebrity = new EventEmitter();
 
 //Subscribe to celebrity for Observer 1
 celebrity.on('race', (result) => {
-    if(result === 'win') {
+    if (result === 'win') {
         console.log('Congratulations! You are the best!');
     }
 });
 
 //Subscribe to celebrity for Observer 2
 celebrity.on('race', (result) => {
-    if(result === 'win') {
+    if (result === 'win') {
         console.log('Boo I could have done better than that!');
     }
 });
@@ -20,5 +20,5 @@ celebrity.emit('race', 'win');
 celebrity.emit('race', 'lose');
 
 process.on('exit', (code) => {
-    console.log('Process exit event with code: ',code);
-})
+    console.log('Process exit event with code: ', code);
+});
