cddc7a6b37c94a57030fbbffc08744acb8ee1f6e
Hung Nguyen <nvhung2711@gmail.com>
2025-08-16 18:19:14 +0930

    initial

---
+++ nasa/server/src/routes/launches/launches.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const { getAllLaunches } = require('./launches.controller');
+
+const launchesRouter = express.Router();
+
+launchesRouter.get('/', getAllLaunches);
+
+module.exports = launchesRouter;
